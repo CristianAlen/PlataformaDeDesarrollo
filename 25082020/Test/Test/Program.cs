3cb7b40e2745b729d1f8f4801f0cfeb139a328d2
Cristian Amarillo <cristian.amarillo96@gmail.com>
2020-08-30 23:58:41 -0300

    Test while

---
+++ 25082020/Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Tienes una caja la cual se abrira si adivinas escribiendo un numero del 1 al 10");
+            Console.WriteLine("Solo tienes 3 intentos!");
+            Console.WriteLine("Hazlo rapido antes de que se escape!");
+            string nro = "";
+            int intentos = 0;
+
+
+            while (nro != "7" && intentos <= 2)
+            {
+                Console.WriteLine("Escribe!");
+                nro = Console.ReadLine();
+                intentos++;
+                Console.WriteLine("Intentos: " + intentos);
+
+               if(nro == "7" && intentos <= 2)
+                {
+                    Console.WriteLine("Abriste la caja, pero no hay nada! :(");
+                    
+                }
+            }
+            if (intentos == 3) { Console.WriteLine("La caja se escapo!"); }
+            
+        }
+    }
+}
