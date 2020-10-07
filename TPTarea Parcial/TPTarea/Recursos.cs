3df31df042ddf2312b80458d38b1b041f331bdd2
CristianAlen <cristian.amarillo96@gmail.com>
2020-10-06 23:15:17 -0300

    sqlite

---
+++ TPTarea Parcial/TPTarea/Recursos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPTarea
+{
+    public class Recursos
+    {
+        public string Nombre { get; set; }
+        public Usuarios Usuarios { get; set; }
+
+    }
+}
