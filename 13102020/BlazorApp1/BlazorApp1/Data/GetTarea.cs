0d6b634e4a6bf7f46e90913d7a3c2a25a013b701
CristianAlen <cristian.amarillo96@gmail.com>
2020-10-13 23:10:49 -0300

    Blazor

---
+++ 13102020/BlazorApp1/BlazorApp1/Data/GetTarea.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class GetTarea
+    {
+        private static readonly string[] Tareas = new[]
+{
+            "Hacer Tarea", "Practicar C#", "Jugar LOL", "Tomar la chocolatada", "Jugar LOL", "Ver Netflix", "A mimir"
+        };
+
+        public TareaModel[] GetTareasAsync()
+        {
+            TareaModel[] resultado = new TareaModel[5];
+
+            resultado[0] = new TareaModel();
+            resultado[0].Titulo = "Titulo 1";
+            resultado[0].Estado = "Titulo 1";
+            resultado[1] = new TareaModel();
+            resultado[0].Titulo = "Titulo 2";
+            resultado[0].Estado = "Titulo 2";
+            resultado[2] = new TareaModel();
+            resultado[0].Titulo = "Titulo 3";
+            resultado[0].Estado = "Titulo 3";
+            resultado[3] = new TareaModel();
+            resultado[0].Titulo = "Titulo 4";
+            resultado[0].Estado = "Titulo 4";
+            resultado[4] = new TareaModel();
+            resultado[0].Titulo = "Titulo 5";
+            resultado[0].Estado = "Titulo 5";
+            resultado[5] = new TareaModel();
+            resultado[0].Titulo = "Titulo 6";
+            resultado[0].Estado = "Titulo 6";
+
+            return resultado;
+        }
+    }
+}
---
+++ 13102020/BlazorApp1/BlazorApp1/Data/TareaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TareaModel
+    {
+       // public int Id { get; set; }
+        public string Titulo { get; set; }
+
+       // public DateTime Vencimiento { get; set; }
+
+        //public int Estimacion { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
